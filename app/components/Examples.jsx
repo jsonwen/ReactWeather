483a80fe7b21b5f26bf1e4ccd09d783929041082
Jason Wen <jason@leadsocial.com>
2016-12-07 17:03:51 +0100

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var Examples = (properties) => {
   return (
-    <h3>Examples!</h3>
+    <div>
+      <h3>Examples!</h3>
+      <p>Welcome to examples page</p>
+    </div>
   )
 }
 
